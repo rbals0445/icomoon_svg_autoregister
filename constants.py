3a54b82f0712c2e793299908827a144032a8061a
hkm <rbals0445@naver.com>
2022-02-17 16:33:23 +0900

    updated: - 함수에 implicit wait 묶었음 - 동적 컨텐츠 확인은 presence_of로 하면 실제 DOM을 체크하므로 안전 - constants에 상수들 뺐음

---
+++ constants.py
@@ -0,0 +1,26 @@
+import os
+## const 영역
+WEB_URL = "https://icomoon.io/app/#/projects"
+PROJECT_IMPORT_DIV = '/html/body/div[4]/div/div[3]/div[2]/div'
+IMPORT_PROJECT_BUTTON = '/html/body/div[4]/div/div[3]/div[2]/div/mi-file/input'
+
+IMPORT_ICON_DIV = '//*[@id="file"]'
+IMPORT_ICON_BUTTON = '//*[@id="file"]/mi-file/input'
+
+IMPORT_FILE_NAME = 'selection.json' # selection.json가 보통 기본 파일명임
+NEW_PROJECT_BUTTON = '/html/body/div[4]/div/div[3]/div[2]/button'
+
+GET_FONT_BUTTON = '/html/body/div[4]/div[1]/div[2]/div[2]/div/a[2]' # 파일 추가후에 누르는 Fonts 버튼
+
+FONTS_DOWNLOAD_BUTTON = '/html/body/div[4]/div[2]/div/div[2]/div/span/button[1]' # 폰트 다운로드 버튼
+
+SHOW_LIGATURE_BUTTON = '//*[@id="toolbar"]/div[2]/div[2]/div/label[2]' #Ligature button 클릭
+
+NEW_PROJECT_LOAD_BUTTON = '/html/body/div[4]/div/div[3]/div[1]/ul/li[2]/fieldset/button[3]' # 새 프로젝트 load 버튼
+
+DRAWER_BUTTON = '//*[@id="setH0"]/button' # select_all 누르기 위한 버튼
+SELECT_ALL_BUTTON = '//*[@id="setH0"]/ul/li[5]/button[1]' 
+## 기타
+BASE_PATH = os.getcwd()
+
+FOLDER_NAME_FOR_ADDITION = "newfiles" # 새로 넣을 이미지를 가지고 있는 폴더 이름
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import chromedriver_autoinstaller
 import zipfile
 import time
 import re
-
+import constants as const
 # TODO
 # python3 main.py로 실행하는경우
 # multicolor일때 search가 너무 늦음.. +1하는 방식이라 그런것같음.
@@ -19,31 +19,9 @@ import re
 # window_size를 변경해서 해결했지만, 스크롤을 움직일 수 있는게 좀 더 근본적 해결일것 같음
 chromedriver_autoinstaller.install()
 
-## const 영역
-WEB_URL = "https://icomoon.io/app/#/projects"
-PROJECT_IMPORT_DIV = '/html/body/div[4]/div/div[3]/div[2]/div'
-IMPORT_PROJECT_BUTTON = '/html/body/div[4]/div/div[3]/div[2]/div/mi-file/input'
-
-IMPORT_ICON_DIV = '//*[@id="file"]'
-IMPORT_ICON_BUTTON = '//*[@id="file"]/mi-file/input'
-
-IMPORT_FILE_NAME = 'selection.json' # selection.json가 보통 기본 파일명임
-LOAD_PROJECT_BUTTON = '/html/body/div[4]/div/div[3]/div[1]/ul/li[2]/fieldset/button[3]'
-
-GET_FONT_BUTTON = '/html/body/div[4]/div[1]/div[2]/div[3]/div/a[2]' # 파일 추가후에 누르는 Fonts 버튼
-
-FONTS_DOWNLOAD_BUTTON = '/html/body/div[4]/div[2]/div/div[2]/div/span/button[1]' # 폰트 다운로드 버튼
-
-SHOW_LIGATURE_BUTTON = '//*[@id="toolbar"]/div[2]/div[2]/div/label[2]' #Ligature button 클릭
-
-## 기타
-BASE_PATH = os.path.dirname(__file__)
-FOLDER_NAME_FOR_ADDITION = "newfiles" # 새로 넣을 이미지를 가지고 있는 폴더 이름
-
 ## 함수
-def setLigatureName(wait) :    
-    wait.until(
-            EC.visibility_of_element_located((By.XPATH, "//*[@id='glyphSet0']/div[1]/div[1]/div/div/span[2]"))) # div버튼 나왔는지 확인
+def setLigatureName() :        
+    isElementPresent("//*[@id='glyphSet0']/div[1]/div[1]/div/div/span[2]")
     x = 1
     while(True) :
         try:
@@ -58,15 +36,6 @@ def setLigatureName(wait) :
             x += 1
         except Exception as e:
             break
-
-def iterateChildSvgComponents(wait) : # 추가된 컴포넌트를 클릭해주는 함수
-    
-    wait.until(
-            EC.visibility_of_element_located((By.XPATH, "//*[@id='set0']/mi-box-selector/div/mi-box[1]"))) # div버튼 나왔는지 확인
-
-    for item in driver.find_elements_by_tag_name('mi-box') :
-        if not item.is_selected() :
-            item.click()
     
 def addMultipleFiles(folderName) : # headless 정상작동
     dirList = os.listdir(folderName)    
@@ -79,19 +48,14 @@ def addMultipleFiles(folderName) : # headless 정상작동
 
 def getBrowserDriver() :    
     op = webdriver.ChromeOptions()
-    #op.add_argument('--disable-dev-shm-usage')
-    #op.add_argument('--no-proxy-server') 
-    #op.add_argument("--proxy-server='direct://'");
-    #op.add_argument("--proxy-bypass-list=*");
-    #op.add_argument("--start-maximized")    
-    op.add_argument("window-size=2560,1440")
+    op.add_argument('--disable-dev-shm-usage')
+    op.add_argument('--no-proxy-server') 
     op.add_argument('headless')
     op.add_argument("disable-gpu") 
-    p = {"download.default_directory": BASE_PATH, "safebrowsing.enabled":False, 'download.prompt_for_download': False }
+    p = {"download.default_directory": const.BASE_PATH, "safebrowsing.enabled":False, 'download.prompt_for_download': False }
     op.add_experimental_option("prefs", p)
     
     driver = webdriver.Chrome(options=op)    
-    print(driver.get_window_size())
     
     return driver;
 
@@ -99,7 +63,7 @@ def checkFileDownloaded() :
     sec = 0
     while sec < 20 :
         time.sleep(1)
-        for fname in os.listdir(BASE_PATH) :
+        for fname in os.listdir(const.BASE_PATH) :
             _,ext = os.path.splitext(fname)
             if ext == ".zip" :
                 return fname
@@ -110,66 +74,57 @@ def checkFileDownloaded() :
 def unzip() :
     zipFileName = checkFileDownloaded()
     
-    fileName = os.path.join(BASE_PATH,zipFileName)
+    fileName = os.path.join(const.BASE_PATH,zipFileName)
     
     with zipfile.ZipFile(fileName,"r") as zip_ref:
-        zip_ref.extractall(os.path.join(BASE_PATH,"results"))
+        zip_ref.extractall(os.path.join(const.BASE_PATH,"results"))
     
     if os.path.isfile(fileName): # zip file 삭제
         os.remove(fileName)
+        
+def isElementClickable(PATH) : # true -> click, false -> exit
+    try :
+        wait.until(
+            EC.element_to_be_clickable((By.XPATH, PATH))).click()
+    except Exception as e :
+        print(PATH,e)
+        return;
+    
+def isElementPresent(PATH) :
+    try :
+        elem = wait.until(
+            EC.presence_of_element_located((By.XPATH, PATH)))
+        return elem
+    except Exception as e :
+        print(PATH,e)
+        return;
 
 def processRequests() : # 최종 실행 함수
     try:
-        wait = WebDriverWait(driver, 30)
+        isElementClickable(const.NEW_PROJECT_BUTTON) # 새 프로젝트 버튼 클릭
+        isElementClickable(const.NEW_PROJECT_LOAD_BUTTON) # 프로젝트에서 Load 버튼 클릭
+        isElementPresent(const.IMPORT_ICON_DIV) # Import Icon 버튼 체크
         
-        wait.until(
-            EC.visibility_of_element_located((By.XPATH, PROJECT_IMPORT_DIV)))
+        addedString = addMultipleFiles(os.path.join(const.BASE_PATH, const.FOLDER_NAME_FOR_ADDITION))
         
-        TEST_FILENAME = os.path.join(BASE_PATH, IMPORT_FILE_NAME)
-        
-
-        driver.find_element(By.XPATH,IMPORT_PROJECT_BUTTON).send_keys(TEST_FILENAME) # input tag 찾아서 파일 넣음.
-        
-        wait.until(
-            EC.element_to_be_clickable((By.XPATH, LOAD_PROJECT_BUTTON))).click() # Load button click
-        
-        wait.until(
-            EC.visibility_of_element_located((By.XPATH, IMPORT_ICON_DIV))) # Import Icon 버튼 체크
-        
-        addedString = addMultipleFiles(os.path.join(BASE_PATH, FOLDER_NAME_FOR_ADDITION))
-        #print(addedString)
-        driver.find_element(By.XPATH,IMPORT_ICON_BUTTON).send_keys(addedString) # folder안에 있는 파일들 업로드 완료
-        
-        wait.until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="set0"]/mi-box-selector/div'))) # div버튼 나왔는지 확인
+        driver.find_element(By.XPATH,const.IMPORT_ICON_BUTTON).send_keys(addedString) # folder안에 있는 파일들 업로드 완료
+                           
+        isElementPresent(const.DRAWER_BUTTON).click() # Drawbtn 찾아서 클릭
+        isElementPresent(const.SELECT_ALL_BUTTON).click() # 전체 선택버튼
+        isElementPresent(const.GET_FONT_BUTTON).click() # Click Font Button
+        isElementClickable(const.SHOW_LIGATURE_BUTTON) # LigatureBtn 나올때까지 대기
+        setLigatureName() # ligature 채움
+        isElementClickable(const.FONTS_DOWNLOAD_BUTTON)
             
-        iterateChildSvgComponents(wait) 
-        
-        wait.until(
-            EC.element_to_be_clickable((By.XPATH, GET_FONT_BUTTON))).click() # Click Font Button
-        
-        wait.until(
-            EC.element_to_be_clickable((By.XPATH, SHOW_LIGATURE_BUTTON))) # LigatureBtn 나올때까지 대기
-        
-        elem = driver.find_element(By.XPATH, SHOW_LIGATURE_BUTTON)        
-        if not elem.is_selected() :
-            elem.click()
-        
-        setLigatureName(wait) # ligature 채움
-        
-        
-        wait.until(
-            EC.element_to_be_clickable((By.XPATH, FONTS_DOWNLOAD_BUTTON))).click() # download button click
-        
-        unzip()
-        
+        unzip()   
     finally:
         print("Update Finished")
         driver.close()
         
 ## 실행 스크립트
 driver = getBrowserDriver()
-driver.get(WEB_URL)
+driver.get(const.WEB_URL)
+wait = WebDriverWait(driver, 30)
 processRequests()
 
 
